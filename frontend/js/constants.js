a741cb1fdf970635ee8745316f79e2fa7ae34190
youngpolyglots@hotmail.com <youngpolyglots@hotmail.com>
2022-04-27 13:28:46 +0200

    New 5

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xAcd5FE80675A8B8424E0d7cBdb8CF3E4DAd603C4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
